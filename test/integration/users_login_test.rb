e568390c3aa23b60d4467f8d742593efd4c27cc9
Giridhar Reddy <giridhar.reddy@effonetech.com>
2016-08-22 18:10:56 +0530

    working on signout module

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,6 +16,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test 'login with valid information followed by logout' do 
     get 'login_path'
     post login_path, params: {session: {email: @user.email,password: 'password'}}
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
