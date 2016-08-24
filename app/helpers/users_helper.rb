4a63f88542e40a8e16b5e745f2a8dcbb1c81dc68
Giridhar Reddy <giridhar.reddy@effonetech.com>
2016-08-24 11:57:39 +0530

    finish edit, update, and destroy actions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-	module UsersHelper
+	
 
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
@@ -7,6 +7,5 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
-end
 
 end
